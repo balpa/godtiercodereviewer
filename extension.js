2e22d74b8a8cc04dafc2fb2eb2165763f0220f03
balpa <berkealtiparmak@outlook.com>
2025-07-18 12:58:59 +0300

    added a button for command

--- extension.js
+++ extension.js
@@ -2,50 +2,56 @@ const vscode = require('vscode');
 const { fixCode } = require('./fixer');
 
 const activate = (context) => {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('godtiercodereviewer.start', async () => {
-			const editor = vscode.window.activeTextEditor;
+	const commandId = 'godtiercodereviewer.start';
 
-			if (!editor) {
-				vscode.window.showErrorMessage('No active editor found!');
+	const disposable = vscode.commands.registerCommand(commandId, async () => {
+		const editor = vscode.window.activeTextEditor;
 
-				return;
-			}
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor found!');
+			return;
+		}
 
-			const selection = editor.selection;
-			let code, range;
+		const selection = editor.selection;
+		let code, range;
 
-			if (selection.isEmpty) {
-				code = editor.document.getText();
+		if (selection.isEmpty) {
+			code = editor.document.getText();
 
-				const start = new vscode.Position(0, 0);
-				const lastLineIndex = editor.document.lineCount - 1;
-				const lastLineLength = editor.document.lineAt(lastLineIndex).text.length;
-				const end = new vscode.Position(lastLineIndex, lastLineLength);
-				range = new vscode.Range(start, end);
-			} else {
-				code = editor.document.getText(selection);
-				range = selection;
-			}
+			const start = new vscode.Position(0, 0);
+			const lastLineIndex = editor.document.lineCount - 1;
+			const lastLineLength = editor.document.lineAt(lastLineIndex).text.length;
+			const end = new vscode.Position(lastLineIndex, lastLineLength);
+			range = new vscode.Range(start, end);
+		} else {
+			code = editor.document.getText(selection);
+			range = selection;
+		}
 
-			if (!code.trim()) {
-				vscode.window.showErrorMessage('No code selected or document is empty!');
+		if (!code.trim()) {
+			vscode.window.showErrorMessage('No code selected or document is empty!');
+			return;
+		}
 
-				return;
-			}
+		const staticallyFixedCode = fixCode(code);
 
-			const staticallyFixedCode = fixCode(code);
+		await editor.edit(editBuilder => {
+			editBuilder.replace(range, staticallyFixedCode);
+		});
 
-			await editor.edit(editBuilder => {
-				editBuilder.replace(range, staticallyFixedCode);
-			});
+		vscode.window.showInformationMessage('Code review done!');
+	});
 
-			vscode.window.showInformationMessage('Code review done!');
-		})
-	)
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 900);
+	statusBarItem.command = commandId;
+	statusBarItem.text = '$(tools) Review Code';
+	statusBarItem.tooltip = 'Run God Tier Code Reviewer';
+	statusBarItem.show();
+
+	context.subscriptions.push(disposable, statusBarItem);
 };
 
-const deactivate = () => {};
+const deactivate = () => { };
 
 module.exports = {
 	activate,
