d6356c02abc80c3c72464600d07f1569dd0b9ef7
balpa <berkealtiparmak@outlook.com>
2025-10-27 18:08:11 +0300

    remove logs imps

--- functions/removeConsoleLog.js
+++ functions/removeConsoleLog.js
@@ -1,5 +1,25 @@
 const recast = require('recast');
-const { namedTypes } = recast.types;
+const { namedTypes: n } = recast.types; 
+
+function isInsideCatchClause(path) {
+    let currentPath = path.parentPath;
+
+    while (currentPath) {
+        const node = currentPath.node;
+
+        if (n.CatchClause.check(node)) {
+            return true;
+        }
+
+        if (n.Function.check(node) || n.Program.check(node)) {
+            return false;
+        }
+
+        currentPath = currentPath.parentPath;
+    }
+
+    return false;
+}
 
 function removeConsoleLog(ast) {
     recast.types.visit(ast, {
@@ -7,22 +27,31 @@ function removeConsoleLog(ast) {
             const { node } = path;
 
             if (
-                namedTypes.CallExpression.check(node.expression) &&
-                namedTypes.MemberExpression.check(node.expression.callee)
+                n.CallExpression.check(node.expression) &&
+                n.MemberExpression.check(node.expression.callee)
             ) {
                 const callee = node.expression.callee;
 
-                const isConsoleLog = 
+                const isConsoleLog =
+                    n.Identifier.check(callee.object) &&
                     callee.object.name === 'console' &&
+                    n.Identifier.check(callee.property) &&
                     callee.property.name === 'log';
 
                 const isInsiderLoggerLog =
-                    callee.object.type === 'MemberExpression' &&
+                    n.MemberExpression.check(callee.object) &&
+                    n.Identifier.check(callee.object.object) &&
                     callee.object.object.name === 'Insider' &&
+                    n.Identifier.check(callee.object.property) &&
                     callee.object.property.name === 'logger' &&
+                    n.Identifier.check(callee.property) &&
                     callee.property.name === 'log';
 
                 if (isConsoleLog || isInsiderLoggerLog) {
+                    if (isInsideCatchClause(path)) {
+                        return this.traverse(path);
+                    }
+
                     path.prune();
 
                     return false;
